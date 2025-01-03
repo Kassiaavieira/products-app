d9590c2c7d6236b980545f975be5d0da430ac383
kassiaavieira <kassiavieira@hotmail.com>
2025-01-03 09:17:32 -0300

    Backend feat: config docker and creation migrate

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
 
-@Controller('products')
-export class ProductsController {}
+@Controller('product')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll() {
+    const products = this.productsService.findAll();
+    return products;
+  }
+
+  @Post()
+  create(@Body() data: { name: string; price: number; amount: number }) {
+    return this.productsService.create(data);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() data: { name?: string; price?: number; amount?: number },
+  ) {
+    return this.productsService.update(Number(id), data);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.productsService.delete(Number(id));
+  }
+}
